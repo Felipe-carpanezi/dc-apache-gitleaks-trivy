1bff822aba54cff3090a576091d71f2c94dc58c9
Felipe-carpanezi <felipecarpanezi@hotmail.com>
2026-09-13 19:14:43 +0000

    feat: initialize secure apache lab

---
+++ app/script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+
+    mensagem.textContent =
+        "JavaScript funcionando dentro da aplicação Docker!";
+}
